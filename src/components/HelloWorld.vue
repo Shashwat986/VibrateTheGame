3cc3d14e898487b9246b70b46c317f4982f50ab2
Shashwat Chandra <chandra.shashwat@gmail.com>
2021-12-09 00:14:33 +0530

    Boilerplate

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <section class="hero">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        Hero title
+      </h1>
+      <h2 class="subtitle">
+        Hero subtitle
+      </h2>
+    </div>
+  </div>
+</section>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
