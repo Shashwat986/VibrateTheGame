8b73f8e733afc2740dacb134d09f5c1348dc22b5
Shashwat Chandra <chandra.shashwat@gmail.com>
2022-01-02 23:26:48 +0530

    Added random levels + Score

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-      <button class="button is-primary is-fullwidth"
+      <button class="button is-primary is-fullwidth is-relative"
               id="main-button"
               @click="buttonClick"
               @mousedown="touchStart"
@@ -8,6 +8,11 @@
               @touchstart="touchStart"
               @touchend="touchEnd">
         <span id="main-button-text" v-html="buttonText()"></span>
+        <div class="is-absolute is-centered" style="top: 0; left: 0; right: 0;">
+          Level: {{ this.displayedLevel }}
+          &#xb7;
+          Tries: {{ tries }}
+        </div>
       </button>
     </div>
 </template>
@@ -15,17 +20,17 @@
 <script>
 
 const STDDEV = 80;
-
 const LEVELS = [
-  [250, 250, 250, 250, 250],
-  [50, 250, 300, 250, 50],
-  [50, 250, 50, 250, 50, 250, 250, 250, 250, 250, 250],
+  [300, 250, 300, 250, 300],
+  [100, 250, 300, 250, 100],
+  [100, 250, 100, 250, 100, 250, 250, 250, 250, 250, 250],
 ]
 
 export default {
   data () {
     return {
       currentLevel: 0,
+      tries: 0,
       currentState: 'TEACH',
       timer: [],
       touchTime: null,
@@ -33,6 +38,9 @@ export default {
     };
   },
   computed: {
+    displayedLevel () {
+      return this.currentLevel + 1
+    },
     matchWidth () {
       return this.timer.slice(
         Math.max(
@@ -42,13 +50,39 @@ export default {
       )
     },
     levelPattern () {
-      return LEVELS[this.currentLevel % LEVELS.length]
+      if (this.currentLevel < LEVELS.length) {
+        return LEVELS[this.currentLevel % LEVELS.length]
+      }
+
+      // Deterministic Pseudo-Random Number Generator
+      // TODO: Make more human-made levels
+
+      let prng = Math.sin(this.currentLevel) * 10000
+      prng -= Math.floor(prng)
+
+      prng *= 10**5
+      prng *= 10**(Math.floor(this.currentLevel / 10))
+
+      let levelSeed = "" + Math.round(prng)
+
+      let rndLevel = []
+      for (let i = 0; i < levelSeed.length; i++) {
+        let ii = parseInt(levelSeed[i])
+
+        let val = [100, 100, 300, 500][ii % 4]
+
+        rndLevel.push(val)
+        rndLevel.push(250)
+      }
+
+      console.log(rndLevel.slice(0, -1))
+      return rndLevel.slice(0, -1)
     }
   },
   methods: {
     buttonText () {
       if (this.currentState == 'TEACH') {
-        return "Level " + (this.currentLevel + 1);
+        return "Level " + this.displayedLevel;
       }
 
       let text = "";
@@ -61,10 +95,11 @@ export default {
         `;
       }
 
-      let idx = 0;
+      let idx = 1;
       for (let i = 0; i < this.matchWidth.length; i++) {
+        idx = 1 - idx;
+
         let tx = ""
-        let uline = false
         let color = 'white'
 
         if (idx == 0) {
@@ -76,29 +111,26 @@ export default {
           } else {
             tx = "&#x2713;"
           }
+
+          if (Math.abs(this.matchWidth[i] - this.levelPattern[i]) > 1.5 * STDDEV) {
+            color = 'danger'
+          } else if (Math.abs(this.matchWidth[i] - this.levelPattern[i]) > STDDEV) {
+            color = 'warning'
+          }
         } else {
           // Pause
-          uline = true
-          if (this.matchWidth[i] - this.levelPattern[i] > STDDEV) {
-            tx = "&#x21AE;"
-          } else if (this.levelPattern[i] - this.matchWidth[i] > STDDEV) {
-            tx = "&harr;"
+
+          if (this.matchWidth[i] - this.levelPattern[i] > 2 * STDDEV) {
+            tx = "&#x2307;"
+            color = 'danger'
           } else {
-            tx = "&#x2713;"
+            continue
           }
         }
 
-        if (Math.abs(this.matchWidth[i] - this.levelPattern[i]) > 1.5 * STDDEV) {
-          color = 'danger'
-        } else if (Math.abs(this.matchWidth[i] - this.levelPattern[i]) > STDDEV) {
-          color = 'warning'
-        }
-
         text += `
-          <span class="has-text-${color} ${uline ? "is-underlined" : ""}">${tx}</span>
+          <span class="has-text-${color}">${tx}</span>
         `;
-
-        idx = 1 - idx;
       }
 
       return text;
@@ -107,6 +139,7 @@ export default {
       if (this.currentState != 'LEARN') {
         this.vibrate();
         this.currentState = 'LEARN';
+        this.tries += 1;
       }
 
       e.preventDefault();
@@ -151,42 +184,61 @@ export default {
 
       e.preventDefault();
     },
-    getStd () {
-      if (this.timer.length < this.levelPattern.length) {
-        return null;
-      }
-
-      let std = 0.0;
-      for (let i = 0; i < this.levelPattern.length; i++) {
-        std += (this.matchWidth[i] - this.levelPattern[i])**2
-      }
-      std = (std / this.levelPattern.length) ** 0.5
-
-      return std;
-
-    },
     validateTime () {
       if (this.timer.length < this.levelPattern.length) {
         return;
       }
 
-      if (this.getStd() < STDDEV) {
+      let std = 0.0;
+      let ct = 0
+      let tooLongPause = false
+      for (let i = 0; i < this.levelPattern.length; i++) {
+        if (i % 2 == 0) {
+          // Touch
+          std += (this.matchWidth[i] - this.levelPattern[i])**2
+          ct += 1
+        } else {
+          // Pause
+          if (this.matchWidth[i] - this.levelPattern[i] > 2 * STDDEV) {
+            tooLongPause = true
+            // Note: Shorter pauses are fine
+          }
+        }
+      }
+      std = (std / ct) ** 0.5
+
+      if (std < STDDEV && !tooLongPause) {
         this.nextLevel()
       } else {
-        this.currentState = 'RETRY'
-        this.pauseTime = null
-        this.touchTime = null
+        this.retry()
       }
     },
+    retry () {
+      navigator.vibrate([100, 120, 100]);
+      this.currentState = 'RETRY'
+      this.pauseTime = null
+      this.touchTime = null
+    },
     nextLevel () {
+      navigator.vibrate(300);
       this.currentLevel += 1;
       this.currentState = 'TEACH'
       this.timer = [];
       this.touchTime = null;
       this.pauseTime = null;
     },
-    vibrate () {
-      navigator.vibrate(this.levelPattern)
+    vibrate (lvl) {
+      if (lvl == null)
+        lvl = this.levelPattern
+
+      let lvlDuration = lvl;
+      if (Array.isArray(lvl)) {
+        lvlDuration = lvl.reduce((a,b) => a+b)
+      }
+
+      console.log(lvlDuration)
+
+      navigator.vibrate(lvl)
     },
   }
 }
@@ -205,5 +257,13 @@ export default {
 
 .is-underlined {
   text-decoration: underline !important;
+}
+
+.is-relative {
+  position: relative !important;
+}
+
+.is-absolute {
+  position: absolute !important;
 }
 </style>
